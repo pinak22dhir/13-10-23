987724ba150660deb28882d9ad8c820129d3f110
pinak dhir <pinakdhir@gmail.com>
2023-10-13 13:57:33 +0530

    patter printing questions

---
+++ basicnumberloop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter n");
+    scanf("%d", &n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;}
+    // in this type of question we will approach like which is changing type that
