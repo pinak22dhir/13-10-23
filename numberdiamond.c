ab918302d46cd8d091bf23aa63b016a1d292306d
pinak dhir <pinakdhir@gmail.com>
2023-10-17 12:05:09 +0530

    star diamond question

--- numberdiamond.c
+++ numberdiamond.c
@@ -4,13 +4,16 @@ int main(){
     printf("enter number");
     scanf("%d", &n);
     int a=1;
+    // int p=1;
     for (int i=1;i<=n;i++){
+         int p=1;
         for(int j=1;j<=n-i;j++)
 {
     printf(" ");
 }  
 for(int k=1;k<=a;k++) {
-    printf("*");
+    printf("%d", p);
+    p++;
    } 
    a=a+2;
 printf("\n");}
